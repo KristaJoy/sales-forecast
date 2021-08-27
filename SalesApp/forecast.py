716d11023b06c7475b1dd1ef634f5a0126176a9b
Krista Joy <krista_joy@hotmail.com>
2021-08-27 09:50:05 -0700

    update look, add new api route

--- SalesApp/forecast.py
+++ SalesApp/forecast.py
@@ -3,7 +3,7 @@ from .app import db
 from sqlalchemy.sql import text
 
 
-#Return data 
+#Return raw sales data 
 def get_region(region):
 
     # all the data
@@ -29,27 +29,17 @@ def get_region(region):
     return sales_data
 
 
-#Return forecast data 
-# def get_forecast(region):
+#Return ml data by REGION
 
-#     # all the data
-#     if region == "National":
-#         sales = db.engine.execute(text('SELECT order_id, order_date, customer_id, segment, city, state, region, product_id, category, subcategory, product_name, sales FROM sales'))
-        
-#         sales_data = []
-#         for s in sales:
-#             salesdict = {'order_id':s[0],'order_date':s[1],'customer_id':s[2],'segment':s[3],'city':s[4],'state':s[5],'region':s[6],'product_id':s[7],'category':s[8],'subcategory':s[9],'product_name':s[10],'sales':s[11] }
-#             sales_data.append(salesdict)
+def get_mlregion(region):
 
+    # filtered by region
+    mlregion = db.engine.execute(text('SELECT date, yhat, region FROM regions WHERE region = :val'), val = region)
         
-#     # filtered by region
-#     else:
-#         sales = db.engine.execute(text('SELECT order_id, order_date, customer_id, segment, city, state, region, product_id, category, subcategory, product_name, sales FROM sales WHERE region = :val'), val = region)
-        
-#         sales_data = []
-#         for s in sales:
-#             salesdict = {'order_id':s[0],'order_date':s[1],'customer_id':s[2],'segment':s[3],'city':s[4],'state':s[5],'region':s[6],'product_id':s[7],'category':s[8],'subcategory':s[9],'product_name':s[10],'sales':s[11] }
-#             sales_data.append(salesdict)
+    mlregion_data = []
+    for m in mlregion:
+        mlregiondict = {'date':m[0],'yhat':m[1],'region':m[2]}
+        mlregion_data.append(mlregiondict)
 
     
-#     return forecast_data
+    return mlregion_data
--- SalesApp/static/app.js
+++ SalesApp/static/app.js
@@ -1,59 +1,68 @@
 ////// Create DROP DOWN MENU ///////////
-const regions = ['National', 'West', 'Central', 'South', 'East']
+const regions = ["National", "West", "Central", "South", "East"];
 
 // Select the drop down menu
 const dropdownMenu = d3.select("#selectRegion");
-  
+
 // assign all the id's to the menu options
-regions.forEach(id => {
-    const selRegion = dropdownMenu.append("option");
-    selRegion.text(id);
-  });
+regions.forEach((id) => {
+  const selRegion = dropdownMenu.append("option");
+  selRegion.text(id);
+});
 
-      
+//////// INIT /////////////
+/// national data here to display on page load
+let selectedOption = regions[0];
 
-  //////// INIT /////////////
-  /// national data here to display on page load
-  let selectedOption = regions[0]
+/// Raw Sales Data API Call
+
+const url = `/api/data?region=${selectedOption}`;
+const url_mlregion = `/api/region?region=${selectedOption}`;
+
+d3.json(url).then(function (data) {
+  d3.json(url_mlregion).then(function (data_mlregion) {
     
-  const url = `/api/data?region=${selectedOption}`;
-  
-  d3.json(url).then(function(response) {
-    
+    console.log(data);
+    console.log(data_mlregion);
+
     /// This block is just test code to see it's working that can be erased
-    console.log(response); // HERE IS WHERE YOU CAN SEE THE DATA BEING BROUGHT IN
     const testrow = d3.select("#chart1");
     testrow.html("");
-    testrow.append("h2").text(response[0]['category']);
-    testrow.append("p").text("hey!")
-    /////
+    testrow.append("h2").text(data[0]["category"]); //Connecting to data from first API call
+    testrow.append("h2").text(data_mlregion[0]["yhat"]); ///Connecting to data from second API call
+    testrow.append("p").text("hey!");
     
-  })
+  });
+});
 
+/////// ON CHANGE //////////
 
-  /////// ON CHANGE //////////
-  
-  // Select the drop down menu
-  d3.selectAll("#selectRegion").on("change", optionChanged);
+// Select the drop down menu
+d3.selectAll("#selectRegion").on("change", optionChanged);
 
-  // Function for change event
-  
-  function optionChanged() {
+// Function for change event
 
-    const dropdownMenuChoice = d3.selectAll("#selectRegion").node();
-    // Assign the dropdown menu option to a variable
-    const selectedOption = dropdownMenuChoice.value;
-    
-    const url = `/api/data?region=${selectedOption}`;
-    
-    d3.json(url).then(function(response) {
+function optionChanged() {
+  const dropdownMenuChoice = d3.selectAll("#selectRegion").node();
+  // Assign the dropdown menu option to a variable
+  const selectedOption = dropdownMenuChoice.value;
+
+  const url = `/api/data?region=${selectedOption}`;
+  const url_mlregion = `/api/region?region=${selectedOption}`;
+
+  d3.json(url).then(function (data) {
+    d3.json(url_mlregion).then(function (data_mlregion) {
       
+      console.log(data);
+      console.log(data_mlregion);
+  
       /// This block is just test code to see it's working that can be erased
-      console.log(response); // HERE IS WHERE YOU CAN SEE THE DATA BEING BROUGHT IN
       const testrow = d3.select("#chart1");
       testrow.html("");
-      testrow.append("h2").text(response[0]['category']);
-      /////
-
-    })
-  }
+      testrow.append("h2").text(data[0]["category"]); //Connecting to data from first API call
+      testrow.append("h2").text(data_mlregion[0]["yhat"]); ///Connecting to data from second API call
+      testrow.append("p").text("whoa!");
+    
+    });
+  });
+}
